d504139ea786f9e4b77118edc496bd14055a5a17
raulgrande83 <raulgrande83@gmail.com>
2016-10-12 19:41:32 +0200

    Change execution

--- src/main/java/Execute.java
+++ src/main/java/Execute.java
@@ -1,14 +1,13 @@
 import java.util.List;
 
 import com.lastminute.flightsearch.beans.Flight;
-import com.lastminute.flightsearch.data.FlightsData;
 import com.lastminute.flightsearch.utils.FlightSearchUtils;
 
 public class Execute {
 
 	public static void main(String[] args) {
 		
-		List<Flight> flights = FlightSearchUtils.getFlightsOriginDestination("BCN", "MAD");
+		List<Flight> flights = FlightSearchUtils.getFlightsOriginDestination("MAD", "BCN");
 		
 		FlightSearchUtils.getAirlineByIATA("IZB");
 		FlightSearchUtils.getAirlineByIATA("IB");
