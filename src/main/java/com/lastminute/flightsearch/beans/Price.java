532fbdcdb4c691469fd1bc9dfb7d1d398120527e
raulgrande83 <raulgrande83@gmail.com>
2016-10-14 13:22:24 +0200

    Changed Money for Price

--- src/main/java/com/lastminute/flightsearch/beans/Price.java
+++ src/main/java/com/lastminute/flightsearch/beans/Price.java
@@ -4,15 +4,28 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Currency;
 
-public class Money {
+/**
+ * This class is used to represent the amount and currency of prices
+ * @author raulgrande83
+ *
+ */
+public class Price {
 
+	//By default the currency used is EURO
     private static final Currency EURO = Currency.getInstance("EUR");
+    //The rounding mode
     private static final RoundingMode DEFAULT_ROUNDING = RoundingMode.HALF_EVEN;
 
+    //Stores the amount of the price
     private BigDecimal amount;
+    //Stores the currency of the price
     private Currency currency;   
 
-    public Money (BigDecimal amount) {
+    /**
+     * Constructor receiving the amount of the price
+     * @param amount
+     */
+    public Price (BigDecimal amount) {
     	this.amount = amount;
     	this.currency = EURO;
     	this.amount = amount.setScale(2, DEFAULT_ROUNDING);
@@ -26,6 +39,9 @@ public class Money {
         return currency;
     }
 
+    /**
+     * Method that prints in format 000.00 €
+     */
     @Override
     public String toString() {
         return getAmount() + " " + getCurrency().getSymbol();
